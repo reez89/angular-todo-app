8d3dc77e21cb0ef04c94552b3de0f1f18ccfa02d
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-08 00:50:21 +0200

    modificato shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingridient } from '../shared/ingridients.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingridients:Ingridient[] = [
+    new Ingridient('Apples',10),
+    new Ingridient('Tomatoes',5),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

eab1daa4d62e22da78ea17e7b324f8d4a2378a97
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-07 23:22:43 +0200

    bozza di dettaglio degli ingredienti

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'
     ),
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'
+    ),
   ]; // per definire il tipo di array, ed associarlo alla classe , useremo quella forma, e il nome Recipe, non è niente altro che il nome del modello dichiarato precedentemente.
 
   constructor() {}

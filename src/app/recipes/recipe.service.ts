06d824e95f1ef9e20d48646d83a9837f26ac6e3a
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-18 16:37:16 +0200

    aggiunto un service che ci permette di comunicare tra i componenti

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,22 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+ private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'
+    ),
+    new Recipe(
+      'Another test Recipe',
+      'This is another simple test with angular',
+      'https://www.inspiredtaste.net/wp-content/uploads/2019/07/Crispy-Falafel-Recipe-2-1200.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); //ritorna un nuovo array identico all'originale.
+  }
+}

e51c52a970dc698ec8d1222894b3cca651067db5
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-24 15:00:34 +0200

    aggiunto routing per edit recipe e new recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -11,9 +12,10 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent},
-    {path: ':id', component: RecipeDetailComponent}
-  ]
-  },
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
+  ]},
   { path: 'shopping-list' , component: ShoppingListComponent }
 ];
 

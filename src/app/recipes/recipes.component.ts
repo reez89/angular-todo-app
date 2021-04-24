52fb47baaa1d541e32a250e2da8f5815e6d6e086
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-24 17:14:21 +0200

    sostituito tutti gli eventi Emitter con Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -10,18 +10,10 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   // tslint:disable-next-line: max-line-length
   // ora che ho creato il mio service, posso configurare un subscribe, in modo tale che reagisca ad ogni cambiamento, al click della mia ricetta in questo caso.
-  ngOnInit(): void {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-        );
-  }
+  ngOnInit(): void {}
 
 }
